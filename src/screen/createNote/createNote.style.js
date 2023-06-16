c3f5adf5dc38d0cba7a5140991d6166ba7f58d98
minnkhantsoe <minnkhantsoe007@gmail.com>
2023-06-17 00:01:56 +0630

    optimizing app

---
+++ src/screen/createNote/createNote.style.js
@@ -0,0 +1,30 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+
+  create_title: {
+    borderColor: "#1aa7ec",
+    borderWidth: 1,
+    paddingHorizontal: 20,
+    marginHorizontal: 10,
+    borderRadius: 50,
+    marginBottom: 20
+  },
+
+  create_body: {
+    borderColor: "#1aa7ec",
+    borderWidth: 1,
+    paddingHorizontal: 20,
+    marginHorizontal: 10,
+    borderRadius: 50,
+    marginBottom: 20
+  },
+
+  note_create_button: {
+    backgroundColor: '#1aa7ec',
+    marginHorizontal: 10,
+    paddingVertical: 10,
+    borderRadius: 50,
+    marginBottom: 20
+  },
+});
