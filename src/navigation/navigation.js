7b6baebadbbb9c6240316dcf30219ca767f6d6fa
minnkhantsoe <minnkhantsoe007@gmail.com>
2023-06-21 17:54:52 +0630

    Adding API

--- src/navigation/navigation.js
+++ src/navigation/navigation.js
@@ -8,6 +8,7 @@ import { FontAwesome } from '@expo/vector-icons';
 import { Ionicons } from '@expo/vector-icons';
 import { MaterialCommunityIcons } from '@expo/vector-icons';
 import EditTask from '../screen/editTask/editTask';
+import { PokemonHome } from '../screen/pokemonHome/pokemonHome';
 
 const Stack = createStackNavigator();
 
@@ -17,9 +18,12 @@ export default function NavigationStack() {
 
     <NavigationContainer>
       <Stack.Navigator>
+
+        <Stack.Screen name="PokemonHome" component={PokemonHome} />
+        
         <Stack.Screen name="Note" component={Home}
           options={{ headerRight: () => (<FontAwesome name="sticky-note-o" size={24} color="#000" style={{ marginRight: 20 }} />) }} />
-
+        
         <Stack.Screen name="CreateNote" component={CreateNote}
           options={{ headerRight: () => (<Ionicons name="create-outline" size={24} color="#000" style={{ marginRight: 20 }} />) }} />
 
